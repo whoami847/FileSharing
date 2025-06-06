d63d45fca4222b14ec4ee2cb3c1ee0907b9cce47
whoami847 <mrbeastbangla069@gmail.com>
2025-06-07 01:34:28 +0600

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -1,197 +1,105 @@
-#(©)AnimeLord_Bots
-#Mehediyt69 on Tg #Dont remove this line
+#
+# Copyright (C) 2025 by AnimeLord-Bots@Github, < https://github.com/AnimeLord-Bots >.
+#
+# This file is part of < https://github.com/AnimeLord-Bots/FileStore > project,
+# and is released under the MIT License.
+# Please see < https://github.com/AnimeLord-Bots/FileStore/blob/master/LICENSE >
+#
+# All rights reserved
+#
 
-import base64
-import re
+import random
+import logging
 import asyncio
-import time
-from pyrogram import filters
-from pyrogram.enums import ChatMemberStatus
-from config import *
-from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
-from shortzy import Shortzy
-from pyrogram.errors import FloodWait
-from database.database import *
+from functools import wraps
+from config import OWNER_ID, START_PIC, RANDOM_IMAGES
+from pyrogram import Client
+from pyrogram.types import Message
+from pyrogram.errors import UserNotParticipant, PeerIdInvalid
+from database.database import db
 
-#
-# Copyright (C) 2025 by AnimeLord-Bots@Github, < https://github.com/AnimeLord-Bots >.
-#
-# This file is part of < https://github.com/AnimeLord-Bots/FileStore > project,
-# and is released under the MIT License.
-# Please see < https://github.com/AnimeLord-Bots/FileStore/blob/master/LICENSE >
-#
-# All rights reserved.
+logger = logging.getLogger(__name__)
 
-#used for cheking if a user is admin ~Owner also treated as admin level
-async def check_admin(filter, client, update):
-    try:
-        user_id = update.from_user.id       
-        return any([user_id == OWNER_ID, await db.admin_exist(user_id)])
-    except Exception as e:
-        print(f"! Exception in check_admin: {e}")
-        return False
+def admin(func):
+    """Decorator to check if the user is an admin."""
+    @wraps(func)
+    async def wrapper(client: Client, message: Message, *args, **kwargs):
+        user_id = message.from_user.id if message.from_user else None
+        if not user_id:
+            await message.reply("<b>❌ User ID not found!</b>")
+            return
+        if user_id == OWNER_ID or await db.admin_exist(user_id):
+            return await func(client, message, *args, **kwargs)
+        else:
+            await message.reply("<b>❌ You are not authorized to use this command!</b>")
+    return wrapper
 
-#
-# Copyright (C) 2025 by AnimeLord-Bots@Github, < https://github.com/AnimeLord-Bots >.
-#
-# This file is part of < https://github.com/AnimeLord-Bots/FileStore > project,
-# and is released under the MIT License.
-# Please see < https://github.com/AnimeLord-Bots/FileStore/blob/master/LICENSE >
-#
-# All rights reserved.
-
-async def is_subscribed(client, user_id):
-    channel_ids = await db.show_channels()
-
-    if not channel_ids:
-        return True
-
+async def is_subscribed(client: Client, user_id: int) -> tuple[bool, list]:
+    """
+    Check if a user is subscribed to all required channels.
+    Returns: (is_subscribed: bool, channels_not_subscribed: list)
+    """
+    logger.info(f"Checking subscription status for user {user_id}")
+    
+    # Skip force-sub check for OWNER_ID
     if user_id == OWNER_ID:
-        return True
+        logger.info(f"User {user_id} is OWNER_ID, skipping force-sub check")
+        return True, []
 
-    for cid in channel_ids:
-        if not await is_sub(client, user_id, cid):
-            # Retry once if join request might be processing
-            mode = await db.get_channel_mode(cid)
-            if mode == "on":
-                await asyncio.sleep(2)  # give time for @on_chat_join_request to process
-                if await is_sub(client, user_id, cid):
-                    continue
-            return False
+    # Check if force-sub system is enabled
+    settings = await db.get_settings()
+    if not settings.get('FORCE_SUB_ENABLED', True):
+        logger.info("Force-sub system is disabled globally")
+        return True, []
 
-    return True
+    channels = await db.show_channels()
+    if not channels:
+        logger.info("No force-sub channels configured")
+        return True, []
 
-#
-# Copyright (C) 2025 by AnimeLord-Bots@Github, < https://github.com/AnimeLord-Bots >.
-#
-# This file is part of < https://github.com/AnimeLord-Bots/FileStore > project,
-# and is released under the MIT License.
-# Please see < https://github.com/AnimeLord-Bots/FileStore/blob/master/LICENSE >
-#
-# All rights reserved.
-
-async def is_sub(client, user_id, channel_id):
-    try:
-        member = await client.get_chat_member(channel_id, user_id)
-        status = member.status
-        logger.debug(f"[SUB] User {user_id} in {channel_id} with status {status}")
-        return status in {
-            ChatMemberStatus.OWNER,
-            ChatMemberStatus.ADMINISTRATOR,
-            ChatMemberStatus.MEMBER
-        }
-
-    except UserNotParticipant:
-        mode = await db.get_channel_mode(channel_id)
-        logger.debug(f"[NOT SUB] User {user_id} not in {channel_id}, mode={mode}")
-        if mode == "on":
-            exists = await db.req_user_exist(channel_id, user_id)
-            logger.debug(f"[REQ] User {user_id} join request for {channel_id}: {exists}")
-            return exists
-        logger.debug(f"[NOT SUB] User {user_id} not in {channel_id} and mode != on")
-        return False
-
-    except Exception as e:
-        logger.error(f"[!] Eʀʀᴏʀ ɪɴ ɪꜱ_ꜱᴜʙ(): {e}")
-        return False
-
-#
-# Copyright (C) 2025 by AnimeLord-Bots@Github, < https://github.com/AnimeLord-Bots >.
-#
-# This file is part of < https://github.com/AnimeLord-Bots/FileStore > project,
-# and is released under the MIT License.
-# Please see < https://github.com/AnimeLord-Bots/FileStore/blob/master/LICENSE >
-#
-# All rights reserved.
-
-async def encode(string):
-    string_bytes = string.encode("ascii")
-    base64_bytes = base64.urlsafe_b64encode(string_bytes)
-    base64_string = (base64_bytes.decode("ascii")).strip("=")
-    return base64_string
-
-async def decode(base64_string):
-    base64_string = base64_string.strip("=") # links generated before this commit will be having = sign, hence striping them to handle padding errors.
-    base64_bytes = (base64_string + "=" * (-len(base64_string) % 4)).encode("ascii")
-    string_bytes = base64.urlsafe_b64decode(base64_bytes) 
-    string = string_bytes.decode("ascii")
-    return string
-
-async def get_messages(client, message_ids):
-    messages = []
-    total_messages = 0
-    while total_messages != len(message_ids):
-        temb_ids = message_ids[total_messages:total_messages+200]
+    not_subscribed = []
+    
+    for channel_id in channels:
         try:
-            msgs = await client.get_messages(
-                chat_id=client.db_channel.id,
-                message_ids=temb_ids
-            )
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
-            msgs = await client.get_messages(
-                chat_id=client.db_channel.id,
-                message_ids=temb_ids
-            )
-        except:
-            pass
-        total_messages += len(temb_ids)
-        messages.extend(msgs)
-    return messages
+            # Skip if channel's display_mode is False
+            display_mode = await db.get_channel_display_mode(channel_id)
+            if not display_mode:
+                logger.info(f"Channel {channel_id} has display_mode off, skipping")
+                continue
+                
+            # Validate channel ID
+            if not isinstance(channel_id, int):
+                logger.warning(f"Invalid channel ID {channel_id}, skipping")
+                continue
+                
+            # Check if bot can access the channel
+            try:
+                chat = await client.get_chat(channel_id)
+            except PeerIdInvalid:
+                logger.error(f"Invalid peer ID for channel {channel_id}")
+                continue
+                
+            # Check if user is a member
+            try:
+                member = await client.get_chat_member(channel_id, user_id)
+                if member.status in ['member', 'administrator', 'creator']:
+                    logger.info(f"User {user_id} is subscribed to channel {channel_id}")
+                    continue
+                else:
+                    logger.info(f"User {user_id} is not subscribed to channel {channel_id}")
+                    not_subscribed.append((channel_id, chat.title, chat.username))
+            except UserNotParticipant:
+                logger.info(f"User {user_id} is not a participant in channel {channel_id}")
+                not_subscribed.append((channel_id, chat.title, chat.username))
+            except Exception as e:
+                logger.error(f"Error checking subscription for channel {channel_id}: {e}")
+                continue
+                
+        except Exception as e:
+            logger.error(f"Unexpected error for channel {channel_id}: {e}")
+            continue
 
-async def get_message_id(client, message):
-    if message.forward_from_chat:
-        if message.forward_from_chat.id == client.db_channel.id:
-            return message.forward_from_message_id
-        else:
-            return 0
-    elif message.forward_sender_name:
-        return 0
-    elif message.text:
-        pattern = r"https://t.me/(?:c/)?(.*)/(\d+)"
-        matches = re.match(pattern,message.text)
-        if not matches:
-            return 0
-        channel_id = matches.group(1)
-        msg_id = int(matches.group(2))
-        if channel_id.isdigit():
-            if f"-100{channel_id}" == str(client.db_channel.id):
-                return msg_id
-        else:
-            if channel_id == client.db_channel.username:
-                return msg_id
-    else:
-        return 0
-
-def get_readable_time(seconds: int) -> str:
-    count = 0
-    up_time = ""
-    time_list = []
-    time_suffix_list = ["s", "m", "h", "days"]
-    while count < 4:
-        count += 1
-        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
-        if seconds == 0 and remainder == 0:
-            break
-        time_list.append(int(result))
-        seconds = int(remainder)
-    hmm = len(time_list)
-    for x in range(hmm):
-        time_list[x] = str(time_list[x]) + time_suffix_list[x]
-    if len(time_list) == 4:
-        up_time += f"{time_list.pop()}, "
-    time_list.reverse()
-    up_time += ":".join(time_list)
-    return up_time
-
-def get_exp_time(seconds):
-    periods = [('days', 86400), ('hours', 3600), ('mins', 60), ('secs', 1)]
-    result = ''
-    for period_name, period_seconds in periods:
-        if seconds >= period_seconds:
-            period_value, seconds = divmod(seconds, period_seconds)
-            result += f'{int(period_value)} {period_name}'
-    return result
+    return (len(not_subscribed) == 0, not_subscribed)
 
 #
 # Copyright (C) 2025 by AnimeLord-Bots@Github, < https://github.com/AnimeLord-Bots >.
@@ -200,22 +108,5 @@ def get_exp_time(seconds):
 # and is released under the MIT License.
 # Please see < https://github.com/AnimeLord-Bots/FileStore/blob/master/LICENSE >
 #
-# All rights reserved.
-
-async def get_shortlink(url, api, link):
-    shortzy = Shortzy(api_key=api, base_site=url)
-    link = await shortzy.convert(link)
-    return link
-
-subscribed = filters.create(is_subscribed)
-admin = filters.create(check_admin)
-
-#
-# Copyright (C) 2025 by AnimeLord-Bots@Github, < https://github.com/AnimeLord-Bots >.
-#
-# This file is part of < https://github.com/AnimeLord-Bots/FileStore > project,
-# and is released under the MIT License.
-# Please see < https://github.com/AnimeLord-Bots/FileStore/blob/master/LICENSE >
-#
-# All rights reserved.
+# All rights reserved
 #
